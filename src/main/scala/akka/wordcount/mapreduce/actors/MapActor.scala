ae6c441a87e4e2aae98b5a0c82c2be2cf2a0f825
Richard Gibson <rickityg@gmail.com>
2013-10-20 20:57:45 +0100

    move map actor functionality to trait

--- src/main/scala/akka/wordcount/mapreduce/actors/MapActor.scala
+++ src/main/scala/akka/wordcount/mapreduce/actors/MapActor.scala
@@ -8,30 +8,37 @@ import akka.wordcount.mapreduce.MapData
  * Date: 06/09/13
  * Time: 21:53
  */
-class MapActor extends Actor {
 
+trait WordFrequencyNode {
   val STOP_WORDS_LIST = List("a", "am", "an", "and", "are", "as", "at",
     "be","do", "go", "if", "in", "is", "it", "of", "on", "the", "to")
+  val SENTENCE_SEPARATOR = " "
 
-     def receive: Receive = {
-    case message: String =>
-      sender ! evaluateExpression(message)
-  }
-  def evaluateExpression(line: String): MapData = MapData {
-    getWordFreq(line.toLowerCase.split(" ").toList)
-}
+  def sentenceAsList(sentence:String) =
+          sentence.toLowerCase.split(SENTENCE_SEPARATOR).toList
 
   def getWordFreq [T](xs: List[String]): Map[String,Int] =
-    pack(xs.sortWith(_<_)
-          .filterNot(STOP_WORDS_LIST.contains(_))
-    ) map(ys => ( ys.head,ys.length )) toMap
+    (pack(xs.sortWith(_ < _)
+      .filterNot(STOP_WORDS_LIST.contains(_))
+    ) map (ys => (ys.head, ys.length))).toMap
 
-
-  def pack[T](xs: List[T]): List[List[T]] = xs match {
+  private def pack[T](xs: List[T]): List[List[T]] = xs match {
     case Nil => Nil
     case x =>
       val (first, rest) = x span (_ == x.head)
       first::pack(rest)
   }
+
+}
+
+class MapActor extends Actor with WordFrequencyNode{
+
+  def receive: Receive = {
+    case message: String =>
+      sender ! evaluateExpression(message)
+  }
+
+  def evaluateExpression(message: String): MapData =
+    MapData (getWordFreq(sentenceAsList(message)))
 
 }
